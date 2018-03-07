7b4330319ee42353eaf679cceab29ae25b18853e
alluans <alluans@yandex.ru>
2018-03-07 09:25:36 +0200

    update structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // { path: 'account', component: UserAccountComponent,
+  //   children: [
+  //     { path: '', redirectTo: '/account/dashboard', pathMatch: 'full'},
+  //     { path: 'dashboard' ,  component: DashboardComponent},
+  //     { path: 'portfolio' ,  component: PortfolioComponent}
+  //   ]
+  // },
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_footer/footer/footer.component';
+import { HeaderComponent } from './_header/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
