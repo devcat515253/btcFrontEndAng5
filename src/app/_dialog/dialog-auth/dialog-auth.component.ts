577f797c6d21de78edfc665833a5d7465561bc1f
h1tan <axe.dota@mail.ru>
2018-03-12 09:20:11 +0200

    [DANIL] добавил код внутри попап

--- src/app/_dialog/dialog-auth/dialog-auth.component.ts
+++ src/app/_dialog/dialog-auth/dialog-auth.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {DialogRegistrationComponent} from '../dialog-registration/dialog-registration.component';
 
 @Component({
   selector: 'app-dialog-auth',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogAuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogAuthComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onYesClick() {
+    this.dialogRef.close({ data: 'data' });
   }
 
 }
